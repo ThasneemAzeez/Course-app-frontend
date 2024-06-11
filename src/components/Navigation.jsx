ffaa16371da311829aead286e7217df583061d73
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-11 21:48:58 +0530

    viewall completed

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+        .container>.row>.col.col-12.col-sm-12.col-
+    </div>
+  )
+}
+
+export default Navigation
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,17 +1,64 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const ViewAll = () => {
-  return (
-    <div>
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    const[data,setData]=useState([])
+    const fetchData=()=>{
+
+        axios.get("",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("succes")
                     
+                } else {
+                    alert("error")
+                }
+            }
+        )
+    }
+    
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Course Title</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Duration</th>
+                                    <th scope="col">Trainer Name</th>
+                                    <th scope="col">Venue</th>
+                                </tr>
+                               
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map(
+                                        (value,index)=>{
+                                           return <tr>
+                                    <th scope="row">{value.coursetitle}</th>
+                                    <td>{value.description}</td>
+                                    <td>{value.date}</td>
+                                    <td>{value.duration}</td>
+                                    <td>{value.venue}</td>
+                                    <td>{value.trainername}</td>
+                                   
+                                </tr>
+                                        }
+                                    )
+                                }
+                                
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ViewAll
